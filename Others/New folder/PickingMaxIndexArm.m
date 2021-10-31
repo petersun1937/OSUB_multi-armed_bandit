61d92fba2cd3e37978097001850f41ababe92ad9
ledzeppelin1201 <60328435+ledzeppelin1201@users.noreply.github.com>
2021-10-30 22:37:50 -0400

    Delete folder

--- Others/New folder/PickingMaxIndexArm.m
+++
@@ -1,9 +0,0 @@
-function ArmToPlay = PickingMaxIndexArm(ucb)
-    m = max(ucb);
-    if ( ~isnan(m));
-        I = find(ucb == m);
-        ArmToPlay = I(1+floor(length(I)*rand)); % Breaking randomly the tie 
-    else
-        ArmToPlay = randi(length(ucb));
-    end
-end
